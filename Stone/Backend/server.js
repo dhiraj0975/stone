3bfbf65ad1a4bb06007bd04ce4154eaf388b7e09
Dhiraj Khobragade <brodj73@gmail.com>
2025-09-19 01:05:19 +0530

    deploy 6

--- Stone/Backend/server.js
+++ Stone/Backend/server.js
@@ -4,8 +4,5 @@ const app = require("./src/app");
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running locally on port ${PORT}`);
 });
-
-module.exports = app;
-
